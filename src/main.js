fc6c387e2aeef98185d3aca784afe2c6f6af3448
tachyond2 <bayesianifeg1@gmail.com>
2024-05-14 20:59:13 +0800

    refactor: automatically regsiter all base component start with prefix 'App'

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ import router from '@/router'
 import AppDate from '@/components/AppDate.vue'
 
 const forumApp = createApp(App)
-forumApp.component('AppDate', AppDate)
+
+const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)/)
+requireComponent.keys().forEach(function(fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName = baseComponentConfig.name || (
+    fileName
+      .replace(/^.+\//, '')
+      .replace(/\.\w+$/, '')
+  )
+  forumApp.component(baseComponentName, baseComponentConfig)
+})
+
 forumApp.use(router).mount('#app')
