3829908bcfaebbb628670b355b3a653a9e194c7b
tachyond2 <bayesianifeg1@gmail.com>
2024-05-15 16:29:59 +0800

    feature(user): add user profile page to allow user to view their profile

---
+++ src/pages/PageProfile.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex-grid">
+    <div class="col-3 push-top">
+
+        <div class="profile-card">
+
+            <p class="text-center">
+                <img :src="user.avatar" :alt="`${user.avatar} profile picture`" class="avatar-xlarge">
+            </p>
+
+            <h1 class="title">{{user.username}}</h1>
+
+            <p class="text-lead">{{user.name}}</p>
+            <p class="text-justify">
+                {{user.bio || 'No bio specified.'}}
+            </p>
+
+            <span class="online">{{ user.username }} is online</span>
+
+            <div class="stats">
+                <span>{{ postsCount }} posts</span>
+                <span>{{ threadsCount }} threads</span>
+            </div>
+
+            <hr>
+
+            <p class="text-large text-center"><i class="fa fa-globe"></i> <a href="#">{{user.website}}</a></p>
+
+        </div>
+
+        <p class="text-xsmall text-faded text-center">Member since june 2003, last visited 4 hours ago</p>
+
+        <div class="text-center">
+          <hr>
+          <a href="edit-profile.html" class="btn-green btn-small">Edit Profile</a>
+        </div>
+
+    </div>
+    <div class="col-7 push-top">
+    <PostList :posts="userPosts"/>
+    </div>
+</div>
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    PostList
+  },
+  computed: {
+    ...mapGetters({ user: 'authUser' }),
+    userPosts() {
+      return this.$store.state.posts.filter(p => p.userId === this.user.id)
+    },
+    postsCount() {
+      return this.userPosts.length
+    },
+    threadsCount() {
+      return this.$store.state.threads.filter(t => t.userId === this.user.id).length
+    }
+  }
+}
+</script>
